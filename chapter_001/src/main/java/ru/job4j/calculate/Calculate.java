28a970f094accdb12c2141ac424050346d43d3c6
Alexander Tsakharias <alexandertsakharias@gmail.com>
2019-05-04 21:06:07 +0300

    1.3. JavaDoc. Code convention[#130616]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,20 @@
+/**
+* Calculator
+*/
 package ru.job4j.calculate;
 
+/**
+* Класс для вывода на консоль надписи "Hello World".
+* @author Alexander Tsakharias
+* @ since 04.05.2019
+* @ version 1
+*/
 public class Calculate {
 	
+	/**
+	* Main
+	* @param args - args.
+	*/
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 	}
