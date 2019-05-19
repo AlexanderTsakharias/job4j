1761709e529da3a8dbb417e1491ad63653376ddf
Alexander Tsakharias <alexandertsakharias@gmail.com>
2019-05-19 12:31:15 +0300

    3.1. Элементарный калькулятор.[#130606]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,9 +2,9 @@ package ru.job4j.calculate;
 
 /**
 * Класс для вывода на консоль надписи "Hello World".
-* @author Alexander Tsakharias
-* @ since 04.05.2019
-* @ version 1
+* @author Alexander Tsakharias (alexandertsakharias@gmail.com)
+* @since 04.05.2019
+* @version 1
 */
 public class Calculate {
 	
--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,49 @@
 package ru.job4j.calculator;
 
+/**
+ * Простой калькулятор
+ * @author Alexander Tsakharias (alexandertsakharias@gmail.com)
+ * @since 19.05.2019
+ * @version 1
+ */
 public class Calculator {
 
+    /**
+     *
+     * @param first
+     * @param second
+     * @return first + second
+     */
     public double add(double first, double second) {
         return first + second;
     }
 
+    /**
+     *
+     * @param first
+     * @param second
+     * @return first - second
+     */
     public double subtract(double first, double second) {
         return first - second;
     }
 
+    /**
+     *
+     * @param first
+     * @param second
+     * @return first / second
+     */
     public double div(double first, double second) {
         return first / second;
     }
 
+    /**
+     *
+     * @param first
+     * @param second
+     * @return first * second
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
